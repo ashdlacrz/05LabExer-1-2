999552fda0e19a4ab8177f2c77efa1b2622618f1
Ashley Dela Cruz <116328595+ashdlacrz@users.noreply.github.com>
2022-11-25 16:41:56 +0800

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreatingFileText1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        void DisplayToList()
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            String path = openFileDialog1.FileName;
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                string _getText = "";
+                while ((_getText = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(_getText);
+                    listView1.Items.Add(_getText);
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmRegistration frmRegistration = new FrmRegistration();
+            frmRegistration.ShowDialog();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DisplayToList();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Successfully uploaded.");
+            listView1.Clear();
+        }
+    }
+}
---
+++ FrmRegistration.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreatingFileText1
+{
+    public partial class FrmRegistration : Form
+    {
+        public static string SetFileName;
+        public FrmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmRegistration frmRegistration = new FrmRegistration();
+            
+            FrmFileName frmFileName = new FrmFileName();
+            frmFileName.ShowDialog();
+
+            long getStudentNo = Convert.ToInt64(textBox1.Text);
+            long getContactNo = Convert.ToInt64(textBox6.Text);
+            int getAge = Convert.ToInt32(textBox5.Text);
+            String getProgram = comboBox1.Text;
+            String getLastName = textBox2.Text;
+            String getFirstName = textBox3.Text;
+            String getMiddleinitial = textBox4.Text;
+            String getGender = comboBox2.Text;
+            String getBirthday = dateTimePicker1.Text;
+
+
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, FrmFileName.SetFileName)))
+            {
+                outputFile.WriteLine("Student No.: " + getStudentNo);
+                outputFile.WriteLine("Full Name: " + getLastName + ", " + getFirstName + ", " + getMiddleinitial);
+                outputFile.WriteLine("Program: " + getProgram);
+                outputFile.WriteLine("Gender: " + getGender);
+                outputFile.WriteLine("Age: " + getAge);
+                outputFile.WriteLine("Birthday: " + getBirthday);
+                outputFile.WriteLine("Contact No.: " + getContactNo);
+
+                Console.WriteLine(getStudentNo);
+                Console.WriteLine(getLastName, getFirstName, getMiddleinitial);
+                Console.WriteLine(getProgram);
+                Console.WriteLine(getGender);
+                Console.WriteLine(getAge);
+                Console.WriteLine(getBirthday);
+                Console.WriteLine(getContactNo);
+            }
+        }
+    }
+}
